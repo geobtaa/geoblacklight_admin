0e06e5acd6a878f736a8e61d962dac763361a089
Eric Larson <ewlarson@gmail.com>
2023-05-12 14:33:20 -0500

    Update template.rb

--- template.rb
+++ template.rb
@@ -1,9 +1,7 @@
 # $ rails _7.0.4.2_ new gbl_admin -m https://raw.githubusercontent.com/geobtaa/geoblacklight_admin/develop/template.rb
 
 gem "blacklight", ">= 7.0", "< 8.0"
-gem "blacklight_advanced_search", "~> 7.0"
 gem "geoblacklight", ">= 4.0"
-gem "statesman", ">= 3.4"
 gem "geoblacklight_admin"
 
 run "bundle install"
