88ff77a4a9aaafe23d9cd2950415546797269a72
Eric Larson <ewlarson@gmail.com>
2023-05-12 14:29:23 -0500

    Update template.rb

--- template.rb
+++ template.rb
@@ -1,4 +1,4 @@
-# $ rails _7.0.4.2_ new app-name -m https://raw.githubusercontent.com/geobtaa/geoblacklight_admin/main/template.rb
+# $ rails _7.0.4.2_ new gbl_admin -m https://raw.githubusercontent.com/geobtaa/geoblacklight_admin/develop/template.rb
 
 gem "blacklight", ">= 7.0", "< 8.0"
 gem "blacklight_advanced_search", "~> 7.0"
@@ -14,3 +14,4 @@ generate "blacklight_advanced_search:install", "--force"
 generate "geoblacklight_admin:install", "--force"
 
 rake "db:migrate"
+rake "db:seed"
