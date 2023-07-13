a4d1241b849747427897052eb0525d314e04a732
Eric Larson <ewlarson@gmail.com>
2023-07-13 11:25:54 -0500

    Update template.rb

--- template.rb
+++ template.rb
@@ -1,14 +1,18 @@
 # frozen_string_literal: true
 
-# $ rails _7.0.4.2_ new gbl_admin -m https://raw.githubusercontent.com/geobtaa/geoblacklight_admin/develop/template.rb
+# $ rails new gbl_admin -m https://raw.githubusercontent.com/geobtaa/geoblacklight_admin/develop/template.rb
 
+gem "devise"
+gem 'devise_invitable', '~> 2.0.0'
 gem "blacklight", ">= 7.0", "< 8.0"
 gem "geoblacklight", ">= 4.0"
 gem "geoblacklight_admin"
 
 run "bundle install"
 
-generate "blacklight:install", "--devise"
+generate "devise:install"
+generate "devise_invitable:install"
+generate "blacklight:install"
 generate "geoblacklight:install", "--force"
 generate "blacklight_advanced_search:install", "--force"
 generate "geoblacklight_admin:install", "--force"
