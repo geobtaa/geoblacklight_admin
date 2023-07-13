ef326cbece55c27fed2d923e90662801c69a3237
Eric Larson <ewlarson@gmail.com>
2023-07-13 15:49:04 -0500

    Update test_helper.rb

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,12 +7,12 @@ ENV["RAILS_ENV"] = "test"
 # require "rails/test_help"
 
 # Load fixtures from the engine
-if ActiveSupport::TestCase.respond_to?(:fixture_path=)
-  ActiveSupport::TestCase.fixture_path = File.expand_path("fixtures", __dir__)
-  ActionDispatch::IntegrationTest.fixture_path = ActiveSupport::TestCase.fixture_path
-  ActiveSupport::TestCase.file_fixture_path = ActiveSupport::TestCase.fixture_path + "/files"
-  ActiveSupport::TestCase.fixtures :all
-end
+#if ActiveSupport::TestCase.respond_to?(:fixture_path=)
+#  ActiveSupport::TestCase.fixture_path = File.expand_path("fixtures", __dir__)
+#  ActionDispatch::IntegrationTest.fixture_path = ActiveSupport::TestCase.fixture_path
+#  ActiveSupport::TestCase.file_fixture_path = ActiveSupport::TestCase.fixture_path + "/files"
+#  ActiveSupport::TestCase.fixtures :all
+#end
 
 require "database_cleaner/active_record"
 DatabaseCleaner.strategy = :truncation
