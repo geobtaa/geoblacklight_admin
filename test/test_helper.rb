78d2ffa39a131c92569564189013f647620c2974
Eric Larson <ewlarson@gmail.com>
2023-07-13 16:07:45 -0500

    Local: tests run green

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,18 +1,18 @@
 # Configure Rails Environment
 ENV["RAILS_ENV"] = "test"
 
-# require_relative "../.internal_test_app/config/environment"
-# ActiveRecord::Migrator.migrations_paths = [File.expand_path("../.internal_test_app/db/migrate", __dir__)]
+require_relative "../.internal_test_app/config/environment"
+ActiveRecord::Migrator.migrations_paths = [File.expand_path("../.internal_test_app/db/migrate", __dir__)]
 # ActiveRecord::Migrator.migrations_paths << File.expand_path("../db/migrate", __dir__)
-# require "rails/test_help"
+require "rails/test_help"
 
 # Load fixtures from the engine
-# if ActiveSupport::TestCase.respond_to?(:fixture_path=)
-#  ActiveSupport::TestCase.fixture_path = File.expand_path("fixtures", __dir__)
-#  ActionDispatch::IntegrationTest.fixture_path = ActiveSupport::TestCase.fixture_path
-#  ActiveSupport::TestCase.file_fixture_path = ActiveSupport::TestCase.fixture_path + "/files"
-#  ActiveSupport::TestCase.fixtures :all
-# end
+if ActiveSupport::TestCase.respond_to?(:fixture_path=)
+  ActiveSupport::TestCase.fixture_path = File.expand_path("fixtures", __dir__)
+  ActionDispatch::IntegrationTest.fixture_path = ActiveSupport::TestCase.fixture_path
+  ActiveSupport::TestCase.file_fixture_path = ActiveSupport::TestCase.fixture_path + "/files"
+  ActiveSupport::TestCase.fixtures :all
+end
 
 require "database_cleaner/active_record"
 DatabaseCleaner.strategy = :truncation
