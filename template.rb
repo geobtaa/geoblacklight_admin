6f5f28cce41bb1257563bc3ad417306f31e9ffda
Eric Larson <ewlarson@gmail.com>
2023-07-14 13:58:49 -0500

    WIP: template install

--- template.rb
+++ template.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
-# $ rails _7.0.4.2_ new gbl_admin -m https://raw.githubusercontent.com/geobtaa/geoblacklight_admin/develop/template.rb
+# $ rails new gbl_admin -m https://raw.githubusercontent.com/geobtaa/geoblacklight_admin/develop/template.rb
 
+gem "devise"
 gem "blacklight", ">= 7.0", "< 8.0"
 gem "geoblacklight", ">= 4.0"
 gem "geoblacklight_admin"
