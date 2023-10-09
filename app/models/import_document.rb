ec0b7665a724f29c83308e65684ec43fd08dab25
Eric Larson <ewlarson@gmail.com>
2023-10-09 14:44:05 -0500

    Update import_document.rb
    Include created_at and updated_at in import_document hash

--- app/models/import_document.rb
+++ app/models/import_document.rb
@@ -14,12 +14,15 @@ class ImportDocument < ApplicationRecord
   end
 
   def to_hash
-    {
+    data_hash = {
       friendlier_id: friendlier_id,
       title: title,
       json_attributes: nullify_empty_json_attributes,
       import_id: import_id
     }
+
+    append_created_at(data_hash)
+    append_updated_at(data_hash)
   end
 
   def nullify_empty_json_attributes
@@ -30,5 +33,21 @@ class ImportDocument < ApplicationRecord
     end
 
     clean_hash
+  end
+
+  def append_created_at(data_hash)
+    if data_hash.has_key?("date_created_dtsi")
+      data_hash.merge!({created_at: data_hash["date_created_dtsi"]})
+    end
+
+    data_hash
+  end
+
+  def append_updated_at(data_hash)
+    if data_hash.has_key?("date_modified_dtsi")
+      data_hash.merge!({updated_at: data_hash["date_modified_dtsi"]})
+    end
+    
+    data_hash
   end
 end
