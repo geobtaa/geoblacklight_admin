a3ff103eedf1da813b38071870aacb62604ff69e
Eric Larson <ewlarson@gmail.com>
2023-12-08 10:44:22 -0600

    Facet by import id

--- lib/generators/geoblacklight_admin/config_generator.rb
+++ lib/generators/geoblacklight_admin/config_generator.rb
@@ -334,5 +334,11 @@ module GeoblacklightAdmin
         "\n      include GeoblacklightAdmin::PublicationStateSearchBehavior"
       end
     end
+
+    def add_import_id_facet
+      inject_into_file "app/controllers/catalog_controller.rb", before: "# Item Relationship Facets" do
+        "\nconfig.add_facet_field Settings.FIELDS.B1G_IMPORT_ID, label: "Import ID", show: false\n"
+      end
+    end
   end
 end
