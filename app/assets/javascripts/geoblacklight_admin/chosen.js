e17b53bbc1d941d83026e49d3aa22bf39a3a7b77
Eric Larson <ewlarson@gmail.com>
2024-03-01 11:27:24 -0600

    JS: Vanilla page listener to fire jQuery chosen.js plugin
    Works, but 'tis a mystery why jQuery events aren't firing.

--- app/assets/javascripts/geoblacklight_admin/chosen.js
+++ app/assets/javascripts/geoblacklight_admin/chosen.js
@@ -1,4 +1,13 @@
-// jQuery Ready
-$(document).on('ready turbolinks:load', function() {
+// @TODO: jQuery events listeners not firing?
+$(document).on('ready', function() {
   $('.chosen-select').chosen();
+});
+
+// @WORKS: Vanilla JS - Calling jQuery Chosen
+document.addEventListener('DOMContentLoaded', function() {
+  var elements = document.querySelectorAll('.chosen-select');
+  Array.prototype.forEach.call(elements, function(el, i){
+    console.log("Vanilla JS Chosen Select");
+    $('.chosen-select').chosen();
+  });
 });
