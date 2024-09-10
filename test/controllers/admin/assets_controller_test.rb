6b4a7dc24bdfb8410c600f44fa92577d06bc74bb
Eric Larson <ewlarson@gmail.com>
2024-09-10 10:38:29 -0500

    StandardRB

--- test/controllers/admin/assets_controller_test.rb
+++ test/controllers/admin/assets_controller_test.rb
@@ -5,12 +5,12 @@ require "test_helper"
 module Admin
   class AssetsControllerTest < ActionDispatch::IntegrationTest
     setup do
-      @asset = assets(:asset_1) 
+      @asset = assets(:asset_1)
 
       get "/users/sign_in"
       sign_in_as users(:user_001)
       post user_session_url
-  
+
       follow_redirect!
       assert_response :success
     end
