2266499cd02cd9c66d0f722819fee4beb15325f7
Eric Larson <ewlarson@gmail.com>
2024-09-12 14:17:36 -0500

    ImportsController - more testing

--- test/controllers/imports_controller_test.rb
+++ test/controllers/imports_controller_test.rb
@@ -2,63 +2,85 @@
 
 require "test_helper"
 
-class ImportsControllerTest < ActionDispatch::IntegrationTest
+class Admin::ImportsControllerTest < ActionDispatch::IntegrationTest
   include Devise::Test::IntegrationHelpers
 
   setup do
-    get "/users/sign_in"
-    sign_in_as users(:user_001)
-    post user_session_url
+    @user = users(:user_001) # Assuming you have a fixture or factory for users
+    sign_in @user
 
-    follow_redirect!
-    assert_response :success
-
-    @import = imports(:one)
+    @import = imports(:one) # Assuming you have a fixture or factory for imports
+    @import_params = {
+      name: "Test Import",
+      filename: "test.csv",
+      source: "local",
+      description: "A test import file",
+      row_count: 100,
+      encoding: "UTF-8",
+      content_type: "text/csv",
+      extension: "csv",
+      type: "Import"
+    }
   end
 
   test "should get index" do
     get admin_imports_url
     assert_response :success
+    assert_template :index
+    assert_not_nil assigns(:imports)
+  end
+
+  test "should show import" do
+    get admin_import_url(@import)
+    assert_response :success
+    assert_template :show
+    assert_not_nil assigns(:import_failed_documents)
+    assert_not_nil assigns(:import_success_documents)
   end
 
   test "should get new" do
     get new_admin_import_url
     assert_response :success
-  end
-
-  test "should create import" do
-    skip("file download missing in test runner")
-    assert_difference("Import.count") do
-      post admin_imports_url, params: {import: {content_type: @import.content_type, description: @import.description, encoding: @import.encoding, extension: @import.extension, filename: @import.filename, headers: @import.headers, name: @import.name, row_count: @import.row_count, source: @import.source, validity: @import.validity, validation_result: @import.validation_result, csv_file: fixture_file_upload("files/btaa_formatted_records.csv", "text/csv"), type: @import.type}}
-    end
-
-    assert_redirected_to admin_import_mappings_url(Import.last)
-  end
-
-  test "should redirect bad headers" do
-    skip("file download missing in test runner")
-    @import = imports(:two)
-    assert_no_difference("Import.count") do
-      post admin_imports_url, params: {import: {content_type: @import.content_type, description: @import.description, encoding: @import.encoding, extension: @import.extension, filename: @import.filename, headers: @import.headers, name: @import.name, row_count: @import.row_count, source: @import.source, validity: @import.validity, validation_result: @import.validation_result, csv_file: fixture_file_upload("files/btaa_formatted_records.csv", "text/csv"), type: @import.type}}
-    end
-  end
-
-  test "should show import" do
-    skip("@TODO: Blob link to test attachment not working?")
-    get admin_import_url(@import)
-    assert_response :success
+    assert_template :new
   end
 
   test "should get edit" do
     get edit_admin_import_url(@import)
     assert_response :success
+    assert_template :edit
+  end
+
+  test "should create import" do
+    skip("@TODO: add file upload to test")
+    assert_difference("Import.count") do
+      post admin_imports_url, params: { import: @import_params }
+    end
+
+    assert_redirected_to admin_import_mappings_path(Import.last)
+    follow_redirect!
+    assert_select "div", text: "Import was successful. Please set your import mapping rules."
+  end
+
+  test "should not create import with invalid data" do
+    post admin_imports_url, params: { import: @import_params.merge(name: "") }
+    assert_response :unprocessable_entity
+    assert_template :new
   end
 
   test "should update import" do
-    skip("file download missing in test runner")
-    patch admin_import_url(@import), params: {import: {content_type: @import.content_type, description: @import.description, encoding: @import.encoding, extension: @import.extension, filename: @import.filename, headers: @import.headers, name: @import.name, row_count: @import.row_count, source: @import.source, validity: @import.validity, validation_result: @import.validation_result, csv_file: fixture_file_upload("files/btaa_formatted_records.csv", "text/csv"), type: @import.type}}
+    skip("@TODO: add file upload to test")
+    patch admin_import_url(@import), params: { import: { name: "Updated Import" } }
+    assert_redirected_to admin_import_path(@import)
+    follow_redirect!
+    assert_select "div", text: "Import was successfully updated."
+    @import.reload
+    assert_equal "Updated Import", @import.name
+  end
 
-    assert_redirected_to admin_import_url(@import)
+  test "should not update import with invalid data" do
+    patch admin_import_url(@import), params: { import: { name: "" } }
+    assert_response :unprocessable_entity
+    assert_template :edit
   end
 
   test "should destroy import" do
@@ -67,12 +89,18 @@ class ImportsControllerTest < ActionDispatch::IntegrationTest
     end
 
     assert_redirected_to admin_imports_url
+    follow_redirect!
+    assert_select "div", text: "Import was successfully destroyed."
   end
 
-  test "validation - no dupliate ids allowed" do
-    skip("file download missing in test runner")
-    assert_no_difference("Import.count") do
-      post admin_imports_url, params: {import: {name: "Test", csv_file: fixture_file_upload("#{Rails.root}/test/fixtures/files/duplicate_ids.csv", "text/csv")}}
+  test "should run import" do
+    skip("@TODO: add file upload to test")
+    # Stub the run! method directly on the @import instance
+    @import.stub(:run!, true) do
+      post run_admin_import_url(@import)
+      assert_redirected_to admin_import_path(@import)
+      follow_redirect!
+      assert_select "div", text: "Import is running. Check back soon for results."
     end
   end
 end
