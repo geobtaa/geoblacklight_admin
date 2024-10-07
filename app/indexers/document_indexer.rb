e559bdeac34f2331d621808f0098e8cf3ea98041
Eric Larson <ewlarson@gmail.com>
2024-10-07 16:19:52 -0500

    Update document_indexer.rb

--- app/indexers/document_indexer.rb
+++ app/indexers/document_indexer.rb
@@ -12,8 +12,10 @@ class DocumentIndexer < Kithe::Indexer
     # to_field 'geomg_id_s', obj_extract('friendlier_id') # the actual db pk, a UUID
 
     # Define `to_field`(s) via Element
-    Element.indexable.each do |elm|
-      to_field elm.solr_field, obj_extract(elm.index_value)
+    if ActiveRecord::Base.connection.table_exists?('elements')
+      Element.indexable.each do |elm|
+        to_field elm.solr_field, obj_extract(elm.index_value)
+      end
     end
 
     to_field "gbl_mdModified_dt", obj_extract("gbl_mdModified_dt")
