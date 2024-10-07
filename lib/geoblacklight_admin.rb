6f67876a5b77b8bacee96df7e7f29f4ac06b0efa
Eric Larson <ewlarson@gmail.com>
2024-10-07 15:22:05 -0500

    Update geoblacklight_admin.rb

--- lib/geoblacklight_admin.rb
+++ lib/geoblacklight_admin.rb
@@ -3,7 +3,12 @@
 require "active_support/dependencies"
 require "geoblacklight_admin/engine"
 require "zeitwerk"
+
+# Try to avoid autoloading the admin controllers
+admin_controllers = "#{__dir__}/app/controllers/admin"
+
 loader = Zeitwerk::Loader.for_gem
+loader.do_not_eager_load(admin_controllers)
 loader.ignore("#{__dir__}/generators")
 loader.setup
 
