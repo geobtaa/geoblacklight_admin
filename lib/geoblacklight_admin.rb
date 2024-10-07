006cead694a26f19bb3d8bdbe88da0b7691b23f4
Eric Larson <ewlarson@gmail.com>
2024-10-07 14:54:11 -0500

    WIP: CI debugging

--- lib/geoblacklight_admin.rb
+++ lib/geoblacklight_admin.rb
@@ -2,6 +2,10 @@
 
 require "active_support/dependencies"
 require "geoblacklight_admin/engine"
+require "zeitwerk"
+loader = Zeitwerk::Loader.for_gem
+loader.ignore("#{__dir__}/generators")
+loader.setup
 
 module GeoblacklightAdmin
 end
