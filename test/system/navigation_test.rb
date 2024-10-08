bd011da06c1ed0ff9df3ce237e8c7c606919258b
Eric Larson <ewlarson@gmail.com>
2024-10-08 10:34:02 -0500

    Update navigation_test.rb

--- test/system/navigation_test.rb
+++ test/system/navigation_test.rb
@@ -13,7 +13,7 @@ class NavigationTest < ApplicationSystemTestCase
       assert page.has_link?("GBL♦Admin")
       assert page.has_selector?("form")
       assert page.has_selector?("input[name='q']")
-      assert page.has_text?("Admin Tools")
+      assert page.has_link?("Admin Tools")
       assert page.has_link?("Documents", visible: false)
       assert page.has_link?("Imports", visible: false)
       assert page.has_link?("Access Links", visible: false)
