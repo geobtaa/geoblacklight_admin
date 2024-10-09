a7484aefea4b6be1c189853e1cf701a6f4fdb6f1
Eric Larson <ewlarson@gmail.com>
2024-10-09 10:30:52 -0500

    Update test_helper.rb
    Fix path for Rails 7.2

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -26,7 +26,7 @@ require "rails/test_help"
 # Load fixtures from the engine
 if ActiveSupport::TestCase.respond_to?(:fixture_paths=)
   ActiveSupport::TestCase.fixture_paths = [File.expand_path("fixtures", __dir__)]
-  ActionDispatch::IntegrationTest.fixture_path = ActiveSupport::TestCase.fixture_paths
+  ActionDispatch::IntegrationTest.fixture_paths = ActiveSupport::TestCase.fixture_paths
   ActiveSupport::TestCase.file_fixture_path = ActiveSupport::TestCase.fixture_paths + ["/files"]
   ActiveSupport::TestCase.fixtures :all
 end
