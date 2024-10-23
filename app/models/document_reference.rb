5032cd994399aff2d32f8a3066dcc4adbbddd281
Eric Larson <ewlarson@gmail.com>
2024-10-22 20:04:38 -0500

    WIP

--- app/models/document_reference.rb
+++ app/models/document_reference.rb
@@ -3,6 +3,24 @@
 require "csv"
 
 # DocumentReference
+#
+# This class represents a reference to a document, which includes a URL and a reference type.
+# It belongs to a document and a reference type, and it supports CSV import and export.
+#
+# Associations:
+# - belongs_to :document
+# - belongs_to :reference_type
+#
+# Callbacks:
+# - after_save :reindex_document
+#
+# Validations:
+# - Validates presence of :friendlier_id, :reference_type_id, :url
+# - Validates uniqueness of :url scoped to :friendlier_id and :reference_type_id
+#
+# Scopes:
+# - to_aardvark_references: Converts references to aardvark format
+# - to_csv: Converts references to CSV format
 class DocumentReference < ApplicationRecord
   belongs_to :document, foreign_key: :friendlier_id, primary_key: :friendlier_id
   belongs_to :reference_type
@@ -12,6 +30,41 @@ class DocumentReference < ApplicationRecord
   validates :friendlier_id, :reference_type_id, :url, presence: true
   validates :url, uniqueness: {scope: [:friendlier_id, :reference_type_id]}
 
+  # Scopes
+  scope :to_aardvark_references, -> {
+    references = where(friendlier_id: pluck(:friendlier_id)).map(&:to_aardvark_reference)
+    merged = {}
+    references.each() do |ref|
+      puts "Ref: #{ref.keys.first}"
+      if ref.keys.first == "http://schema.org/downloadUrl"
+        merged["http://schema.org/downloadUrl"] ||= []
+        merged["http://schema.org/downloadUrl"] << {
+          "url" => ref.values.first,
+          "label" => ref[:label]
+        }
+      else
+        merged[ref.keys.first] = ref.values.first
+      end
+    end
+    merged
+  }
+  scope :to_csv, -> { where(friendlier_id: pluck(:friendlier_id)).map(&:to_csv) }
+
+  # CSV Column Names
+  #
+  # Returns an array of column names for CSV export.
+  #
+  # @return [Array<String>] the CSV column names
+  def self.csv_column_names
+    ["friendlier_id", "reference_type", "reference_url", "label"]
+  end
+
+  # Import
+  #
+  # Imports document references from a CSV file.
+  #
+  # @param file [File] the CSV file to import
+  # @return [Boolean] true if import is successful
   def self.import(file)
     logger.debug("CSV Import")
     ::CSV.foreach(file.path, headers: true) do |row|
@@ -33,6 +86,12 @@ class DocumentReference < ApplicationRecord
     true
   end
 
+  # Destroy All
+  #
+  # Destroys document references based on a CSV file.
+  #
+  # @param file [File] the CSV file to process
+  # @return [Boolean] true if destroy is successful
   def self.destroy_all(file)
     logger.debug("CSV Destroy")
     ::CSV.foreach(file.path, headers: true) do |row|
@@ -51,14 +110,46 @@ class DocumentReference < ApplicationRecord
     true
   end
 
+  # To CSV
+  #
+  # Converts the document reference to an array suitable for CSV export.
+  #
+  # @return [Array<String>] the CSV row data
   def to_csv
-    attributes.values
+    [
+      friendlier_id,
+      reference_type.name,
+      url,
+      label
+    ]
   end
 
+  # To Aardvark Reference
+  #
+  # Converts the document reference to an aardvark reference format.
+  #
+  # @return [Hash] the aardvark reference
+  def to_aardvark_reference
+    hash = {}
+    hash[reference_type.reference_uri.to_s] = url
+    hash[:label] = label if reference_type.reference_uri.to_s == "http://schema.org/downloadUrl"
+    hash
+  end
+
+  # Reindex Document
+  #
+  # Reindexes the associated document.
   def reindex_document
     document.save
   end
 
+  # Convert Import Row
+  #
+  # Converts a CSV row to an array of attributes for a document reference.
+  #
+  # @param row [CSV::Row] the CSV row to convert
+  # @return [Array] the converted row data
+  # @raise [RuntimeError] if the reference type is not found
   def self.convert_import_row(row)
     reference_type = ReferenceType.find_by(name: row[1])
     raise "ReferenceType not found" unless reference_type
---
+++ app/services/export_csv_document_references_service.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+require "csv"
+
+# ExportCsvDocumentReferencesService
+class ExportCsvDocumentReferencesService
+  def self.short_name
+    "Document References"
+  end
+
+  def self.call(document_ids)
+    ActionCable.server.broadcast("export_channel", {progress: 0})
+
+    document_ids = document_ids.flatten
+    total = document_ids.size
+    count = 0
+    slice_count = 100
+    csv_file = []
+
+    Rails.logger.debug { "\n\nExportCsvDocumentReferencesService: #{document_ids.inspect}\n\n" }
+
+    CSV.generate(headers: true) do |_csv|
+      csv_file << DocumentReference.csv_column_names
+      document_ids.each_slice(slice_count) do |slice|
+        # Broadcast progress percentage
+        count += slice_count
+        progress = ((count.to_f / total) * 100).round
+        progress = 100 if progress > 100
+
+        ActionCable.server.broadcast("export_channel", {progress: progress})
+        slice.each do |doc_id|
+          doc = Document.find_by(friendlier_id: doc_id)
+
+          Rails.logger.debug { "\n\nDocReferences: #{doc.document_references.size}\n\n" }
+
+          doc.document_references.each do |reference|
+            csv_file << reference.to_csv
+          end
+        rescue NoMethodError
+          Rails.logger.debug { "\n\nExport Failed: #{doc_id.inspect}\n\n" }
+        end
+      end
+    end
+
+    csv_file
+  end
+end
