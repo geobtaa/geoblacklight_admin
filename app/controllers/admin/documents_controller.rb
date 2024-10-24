f5379acf3162c2f773a120fe4cc31da263a22a60
Eric Larson <ewlarson@gmail.com>
2024-10-24 15:29:20 -0500

    RDoc: write RDoc for all GBL Admin controllers

--- app/controllers/admin/documents_controller.rb
+++ app/controllers/admin/documents_controller.rb
@@ -1,17 +1,25 @@
 # frozen_string_literal: true
 
 # Admin::DocumentsController
+# This controller handles the management of documents within the admin interface.
+# It provides actions to list, create, update, and delete documents, as well as
+# export them in various formats.
 module Admin
   class DocumentsController < Admin::AdminController
+    # Allow all parameters (not recommended for production use)
     ActionController::Parameters.permit_all_parameters = true
-    before_action :set_document,
-      only: %i[show edit update destroy admin versions]
+
+    # Set the document before certain actions
+    before_action :set_document, only: %i[show edit update destroy admin versions]
 
     # GET /documents
     # GET /documents.json
+    # Lists all documents with support for various export formats.
     def index
+      # Construct the request URL
       @request = "#{request.protocol}#{request.host}:#{request.port}"
 
+      # Define query parameters for the document search
       query_params = {
         q: params["q"],
         f: params["f"],
@@ -22,54 +30,38 @@ module Admin
       }
       @documents = BlacklightApi.new(@request, **query_params)
 
+      # Respond to different formats
       respond_to do |format|
         format.html { render :index }
         format.json { render json: @documents.results.to_json }
-
-        # JSON - BTAA Aardvark
         format.json_btaa_aardvark do
-          ExportJsonJob.perform_later(@request, current_user, query_params.merge!({format: "json_btaa_aardvark"}),
-            ExportJsonService)
+          ExportJsonJob.perform_later(@request, current_user, query_params.merge!({format: "json_btaa_aardvark"}), ExportJsonService)
           head :no_content
         end
-
-        # JSON - GBL Aardvark
         format.json_aardvark do
           ExportJsonJob.perform_later(@request, current_user, query_params.merge!({format: "json_aardvark"}), ExportJsonService)
           head :no_content
         end
-
-        # JSON - GBL v1
         format.json_gbl_v1 do
           ExportJsonJob.perform_later(@request, current_user, query_params.merge!({format: "json_gbl_v1"}), ExportJsonService)
           head :no_content
         end
-
-        # JSON - FILE
         format.json_file do
           ExportJsonBulkJob.perform_later(@request, current_user, query_params.merge!({format: "json_file"}), ExportJsonService)
           head :no_content
         end
-
-        # CSV - B1G
         format.csv do
           ExportJob.perform_later(@request, current_user, query_params, ExportCsvService)
           head :no_content
         end
-
-        # CSV Document Downloads - B1G
         format.csv_document_downloads do
           ExportJob.perform_later(@request, current_user, query_params, ExportCsvDocumentDownloadsService)
           head :no_content
         end
-
-        # CSV Document Access Links - B1G
         format.csv_document_access_links do
           ExportJob.perform_later(@request, current_user, query_params, ExportCsvDocumentAccessLinksService)
           head :no_content
         end
-
-        # CSV Document References - B1G
         format.csv_document_references do
           ExportJob.perform_later(@request, current_user, query_params, ExportCsvDocumentReferencesService)
           head :no_content
@@ -77,7 +69,8 @@ module Admin
       end
     end
 
-    # Fetch documents from array of friendlier_ids
+    # Fetch documents from an array of friendlier_ids
+    # This action retrieves documents based on their friendlier IDs.
     def fetch
       @request = "#{request.protocol}#{request.host}:#{request.port}"
       @documents = Document.where(friendlier_id: params["ids"])
@@ -85,78 +78,62 @@ module Admin
       respond_to do |format|
         format.html { render :index }
         format.json { render json: @documents.to_json }
-
-        # JSON - BTAA Aardvark
         format.json_btaa_aardvark do
-          ExportJsonJob.perform_later(@request, current_user, {ids: @documents.pluck(:friendlier_id), format: "json_btaa_aardvark"},
-            ExportJsonService)
+          ExportJsonJob.perform_later(@request, current_user, {ids: @documents.pluck(:friendlier_id), format: "json_btaa_aardvark"}, ExportJsonService)
           head :no_content
         end
-
-        # JSON - GBL Aardvark
         format.json_aardvark do
-          ExportJsonJob.perform_later(@request, current_user, {ids: @documents.pluck(:friendlier_id), format: "json_aardvark"},
-            ExportJsonService)
+          ExportJsonJob.perform_later(@request, current_user, {ids: @documents.pluck(:friendlier_id), format: "json_aardvark"}, ExportJsonService)
           head :no_content
         end
-
-        # JSON - GBL v1
         format.json_gbl_v1 do
-          ExportJsonJob.perform_later(@request, current_user, {ids: @documents.pluck(:friendlier_id), format: "json_gbl_v1"},
-            ExportJsonService)
+          ExportJsonJob.perform_later(@request, current_user, {ids: @documents.pluck(:friendlier_id), format: "json_gbl_v1"}, ExportJsonService)
           head :no_content
         end
-
-        # CSV - B1G
         format.csv do
-          ExportJob.perform_later(@request, current_user, {ids: @documents.pluck(:friendlier_id), format: "csv"},
-            ExportCsvService)
+          ExportJob.perform_later(@request, current_user, {ids: @documents.pluck(:friendlier_id), format: "csv"}, ExportCsvService)
           head :no_content
         end
-
-        # CSV Document Downloads - B1G
         format.csv_document_downloads do
-          ExportJob.perform_later(@request, current_user,
-            {ids: @documents.pluck(:friendlier_id), format: "csv_document_downloads"}, ExportCsvDocumentDownloadsService)
+          ExportJob.perform_later(@request, current_user, {ids: @documents.pluck(:friendlier_id), format: "csv_document_downloads"}, ExportCsvDocumentDownloadsService)
           head :no_content
         end
-
-        # CSV Document Downloads - B1G
         format.csv_document_access_links do
-          ExportJob.perform_later(@request, current_user,
-            {ids: @documents.pluck(:friendlier_id), format: "csv_document_access_links"}, ExportCsvDocumentAccessLinksService)
+          ExportJob.perform_later(@request, current_user, {ids: @documents.pluck(:friendlier_id), format: "csv_document_access_links"}, ExportCsvDocumentAccessLinksService)
           head :no_content
         end
-
-        # CSV Document References - B1G
         format.csv_document_references do
-          ExportJob.perform_later(@request, current_user,
-            {ids: @documents.pluck(:friendlier_id), format: "csv_document_references"}, ExportCsvDocumentReferencesService)
+          ExportJob.perform_later(@request, current_user, {ids: @documents.pluck(:friendlier_id), format: "csv_document_references"}, ExportCsvDocumentReferencesService)
           head :no_content
         end
       end
     end
 
     # GET /documents/new
+    # Renders a form for creating a new document.
     def new
       @document = Document.new
       render :edit
     end
 
     # GET /documents/1/edit
+    # Renders a form for editing an existing document.
     def edit
     end
 
     # GET /documents/1/admin
+    # Admin view for a specific document.
     def admin
     end
 
     # GET /documents/1/versions
+    # Displays the version history of a document.
     def versions
     end
 
     # POST /documents
     # POST /documents.json
+    # Creates a new document with the provided parameters.
     def create
       @document = Document.new(document_params)
       @document.friendlier_id = @document.send(GeoblacklightAdmin::Schema.instance.solr_fields[:id])
@@ -173,6 +150,7 @@ module Admin
 
     # PATCH/PUT /documents/1
     # PATCH/PUT /documents/1.json
+    # Updates an existing document with the provided parameters.
     def update
       respond_to do |format|
         if @document.update(document_params)
@@ -187,6 +165,7 @@ module Admin
 
     # DELETE /documents/1
     # DELETE /documents/1.json
+    # Deletes a document.
     def destroy
       @document.destroy
       respond_to do |format|
@@ -197,6 +176,8 @@ module Admin
       end
     end
 
+    # GET /documents/1
+    # Shows a document in various formats.
     def show
       respond_to do |format|
         format.html { redirect_to edit_admin_document_url(@document) }
@@ -204,37 +185,29 @@ module Admin
         format.json_aardvark
         format.json_btaa_aardvark
         format.json_gbl_v1
-        # B1G CSV
         format.csv { send_data collect_csv([@document]), filename: "documents-#{Time.zone.today}.csv" }
-
-        # @TODO:
-        # geoblacklight_version: 1.0 (strict)
-        # geoblacklight_version: 1.0 + B1G customizations
-        # geoblacklight_version: 2.0 (strict)
-        # geoblacklight_version: 2.0 + B1G customizations
       end
     end
 
     private
 
     # Use callbacks to share common setup or constraints between actions.
+    # Finds and sets the document based on the friendlier_id.
     def set_document
       @document = Document.includes(:leaf_representative).find_by!(friendlier_id: params[:id] || params[:document_id])
     end
 
-    # only allow whitelisted params through (TODO, we're allowing all document params!)
-    # Plus sanitization or any other mutation.
-    #
-    # This could be done in a form object or otherwise abstracted, but this is good
-    # enough for now.
+    # Defines the list of permitted parameters for document creation and updates.
     def permittable_params
       %i[title publication_state layer_geom_type_s dct_references_s q f page sort rows daterange]
     end
 
+    # Strong parameters for document creation and updates.
     def document_params
       Kithe::Parameters.new(params).require(:document).permit_attr_json(Document).permit(permittable_params)
     end
 
+    # Collects documents into a CSV format.
     def collect_csv(documents)
       CSV.generate(headers: true) do |csv|
         csv << GeoblacklightAdmin::Schema.instance.exportable_fields.map { |k, _v| k.to_s }
