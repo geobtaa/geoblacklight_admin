60b957d7fa450a76af6ee9ed5e24e87e1f300620
Eric Larson <ewlarson@gmail.com>
2024-12-19 13:55:16 -0600

    WIP 3: basic support for entries

--- lib/generators/geoblacklight_admin/config_generator.rb
+++ lib/generators/geoblacklight_admin/config_generator.rb
@@ -220,6 +220,13 @@ module GeoblacklightAdmin
                 get "destroy_all"
                 post "destroy_all"
               end
+
+              resources :document_data_dictionary_entries, path: "entries" do
+                collection do
+                  get "destroy_all"
+                  post "destroy_all"
+                end
+              end
             end
 
             # DocumentDownloads
