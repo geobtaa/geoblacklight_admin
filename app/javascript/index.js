e5b6c8753988465e99981da37e1a2497b100e5af
Eric Larson <ewlarson@gmail.com>
2025-01-15 13:40:03 -0600

    NPM: drop tom-select

--- app/javascript/index.js
+++ app/javascript/index.js
@@ -3,14 +3,12 @@ console.log('Vite ⚡️ Rails - GBL Admin')
 // Import Stimulus and controllers
 import { Application } from '@hotwired/stimulus'
 import ResultsController from "./controllers/results_controller"
-import TagInputController from "./controllers/tag_input_controller"
 
 // Initialize Stimulus
 window.Stimulus = Application.start()
 
 // Register controllers
 Stimulus.register("results", ResultsController)
-Stimulus.register("taginput", TagInputController)
 
 // Import channels
 import './channels';
