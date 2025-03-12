abe25c1b0bbf3a27a928e2466cdcc23efc32ef67
Eric Larson <ewlarson@gmail.com>
2025-03-12 16:01:37 -0500

    Update document_data_dictionary.rb
    Only validate CSV file if present

--- app/models/document_data_dictionary.rb
+++ app/models/document_data_dictionary.rb
@@ -15,7 +15,7 @@ class DocumentDataDictionary < ApplicationRecord
 
   # Validations
   validates :name, presence: true
-  validates :csv_file, attached: true, content_type: {in: "text/csv", message: "is not a CSV file"}
+  validates :csv_file, content_type: { in: "text/csv", message: "is not a CSV file" }, if: -> { csv_file.attached? }
 
   validates_with DocumentDataDictionary::CsvHeaderValidator
 
