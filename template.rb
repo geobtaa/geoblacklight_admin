7ddd1c2425bbcc64eacdf9cbf378ef5ddd57ff9f
Eric Larson <ewlarson@gmail.com>
2025-04-16 11:09:01 -0500

    Update template.rb
    Simplify

--- template.rb
+++ template.rb
@@ -20,48 +20,39 @@ gem "blacklight", ">= 7.0", "< 8.0"
 gem "geoblacklight", ">= 4.0"
 gem "geoblacklight_admin", git: "git@github.com:geobtaa/geoblacklight_admin.git", branch: "develop"
 
-# Inject geoblacklight_admin dependencies
-def inject_geoblacklight_admin_dependencies
-  require 'open-uri'
-  
-  gemspec_url = "https://raw.githubusercontent.com/geobtaa/geoblacklight_admin/develop/geoblacklight_admin.gemspec"
-  
-  begin
-    gemspec_content = URI.open(gemspec_url).read
-    
-    # Match lines like: s.add_dependency "gem_name", "~> 1.0"
-    dependency_pattern = /^\s*s\.add_dependency\s*["']([^"']+)["'](,\s*["'][^"']+["'])?/
-    
-    gemspec_content.each_line do |line|
-      if match = line.match(dependency_pattern)
-        name = match[1]
-        version = match[2]&.gsub(/[",\s]/, '') # Clean up version constraint
-        
-        # Skip if the dependency is already declared
-        next if gem_already_declared?(name)
-        
-        if version
-          say "Adding dependency: #{name} (#{version})", :green
-          gem name, version
-        else
-          say "Adding dependency: #{name}", :green
-          gem name
-        end
-      end
-    end
-  rescue OpenURI::HTTPError => e
-    say "Warning: Could not fetch gemspec from #{gemspec_url}: #{e.message}", :red
-  rescue StandardError => e
-    say "Warning: Error processing gemspec: #{e.message}", :red
-  end
-end
-
-def gem_already_declared?(name)
-  # Check if gem is already declared in template
-  File.readlines(__FILE__).any? { |line| line.match?(/^\s*gem\s+["']#{name}["']/) }
-end
-
-inject_geoblacklight_admin_dependencies
+# GBL‡ADMIN
+# Add geoblacklight_admin runtime dependencies
+gem "active_storage_validations", "~> 1.0"
+gem "amazing_print"
+gem "blacklight", "~> 7.0"
+gem "blacklight_advanced_search"
+gem "blacklight_range_limit"
+gem "bootstrap", "~> 4.0"
+gem "chosen-rails", "~> 1.10"
+gem "cocoon", "~> 1.2"
+gem "config", "~> 4.0"
+gem "devise", "~> 4.7"
+gem "devise-bootstrap-views", "~> 1.0"
+gem "dotenv-rails", "~> 2.8"
+gem "geoblacklight", "~> 4.0"
+gem "haml", "~> 5.2"
+gem "httparty", "~> 0.21"
+gem "inline_svg", "~> 1.9"
+gem "jquery-rails", "~> 4.4"
+gem "kithe", "~> 2.0"
+gem "mutex_m", "~> 0.2.0"
+gem "noticed", "~> 1.6"
+gem "pagy", "~> 9.0"
+gem "paper_trail", "~> 15.0"
+gem "pg", "~> 1.4"
+gem "qa", "~> 5.0"
+gem "ruby-progressbar"
+gem "simple_form", "~> 5.0"
+gem "sprockets", "~> 3.0"
+gem "statesman", "~> 12.0"
+gem "vite_rails", "~> 3.0"
+gem "vite_ruby", ">= 3.5"
+gem "zeitwerk", "~> 2.6"
 
 run "bundle install"
 
